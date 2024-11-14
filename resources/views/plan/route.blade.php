4cb873486e6b6b2cca5acf57ee75742aaee300fe
itaivincent <itaivincent321@gmail.com>
2024-11-14 15:00:55 +0200

    done

--- resources/views/plan/route.blade.php
+++ resources/views/plan/route.blade.php
@@ -67,7 +67,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="selectDateModalLabel">Select a Date</h5>
+                <h5 class="modal-title" id="selectDateModalLabel">Make Your Plan</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
