2e9622efc890f9f5672b774c706c061ba63c58fb
itaivincent <itaivincent321@gmail.com>
2024-11-20 10:41:36 +0200

    users

--- resources/views/users/parameters.blade.php
+++ resources/views/users/parameters.blade.php
@@ -26,46 +26,7 @@
 
                     <div class="row layout-top-spacing">
                     
-                        
-                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 layout-spacing">
-                            <div class="widget widget-card-four">
-                                <div class="widget-content">
-                                    <div class="w-header">
-                                        <div class="w-info">
-                                            <h6 class="value">Modules</h6>
-                                        </div>
-                                        <div class="task-action">
-                                            <div class="dropdown">
-                                                <a class="dropdown-toggle" href="#" role="button" id="expenses" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>
-                                                </a>
-
-                                                <div class="dropdown-menu left" aria-labelledby="expenses" style="will-change: transform;">
-                                                    <a class="dropdown-item" href="javascript:void(0);">Create Module</a>
-                                                    <a class="dropdown-item" href="javascript:void(0);">View Module </a>
-                                                   
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="w-content">
-
-                                        <div class="w-info">
-                                            <p class="value"><span></span> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trending-up"><polyline points="23 6 13.5 15.5 8.5 10.5 1 18"></polyline><polyline points="17 6 23 6 23 12"></polyline></svg></p>
-                                        </div>
-                                        
-                                    </div>
-
-                                    <div class="w-progress-stats">                                            
-                                     
-                                  
-                                        
-                                    </div>
-                                </div>
-                            </div>
-                        </div>  
-
+                    
                         <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 layout-spacing">
                             <div class="widget widget-card-four">
                                 <div class="widget-content">
