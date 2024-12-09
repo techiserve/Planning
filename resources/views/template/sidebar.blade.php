fc1e38de73985e6ed2317fdd0323183497ebaed3
itaivincent <itaivincent321@gmail.com>
2024-12-09 11:49:57 +0200

    done

--- resources/views/template/sidebar.blade.php
+++ resources/views/template/sidebar.blade.php
@@ -30,7 +30,7 @@
                     <li class="menu">                  
                         <a href="#plan" data-bs-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                             <div class="">
-                                <span>Planning.</span>
+                                <span>Planning</span>
                             </div>
                             <div>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
